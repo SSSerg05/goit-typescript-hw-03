42ef56532d3e040861b4db9f8e50bcec890864ea
SSSerg05 <ssserg05@gmail.com>
2023-10-31 22:38:54 +0200

    add 1,2
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/2.ts
+++ src/2.ts
@@ -16,24 +16,22 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string, 
+    private department: string, 
+    protected salary: number) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
-class Manager extends Employee {
+class Manager extends Employee { 
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(protected salary: number) {
+    super('John', 'tecnical', 100); // Виклик батьківського конструктора
+    this.salary += 10000;
+  }
 }
-
 
 export {};
