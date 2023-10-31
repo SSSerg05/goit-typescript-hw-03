4a5d691c3d2e0fee320d833a40b0d3bf41f939e8
SSSerg05 <ssserg05@gmail.com>
2023-10-31 23:26:56 +0200

    add 3
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/3.ts
+++ src/3.ts
@@ -6,6 +6,16 @@
   Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce: () => {};
+  levelUp: () => {};
+}
+
+interface ISpellCaster {
+  castSpell: () => {};
+}
 
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
