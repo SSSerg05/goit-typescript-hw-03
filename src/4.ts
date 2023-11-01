deb4d7ca8b8835641bf4aa7c2637f4ea8d1e052d
SSSerg05 <ssserg05@gmail.com>
2023-11-01 10:05:37 +0200

    add 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  constructor(private signature: number = 0) {
+    this.signature = Math.random()*10;
+  }
+
+  public getSignature = ():number => {
+    return this.signature
+  }
+}
+
+class Person {
+  constructor(private key: Key){}
+
+  public getKey = ():Key => {
+    console.log(`Person take key: ${this.key.getSignature()}`);
+    return this.key;
+  }
+}
+
+abstract class House {
+  constructor(
+    private key: Key,
+    public door: true | false = false,
+    public tenants: Person[] = []
+  ) { }
+  
+  public comeIn = (person:Person) => {
+    if (this.door) {
+      this.tenants.push(person);
+
+      const last = this.tenants.length-1
+      console.log(`Person with key:${this.tenants[last].getKey().getSignature()} open door in Home`);
+    }
+  }
+
+  abstract openDoor(key:Key): void;
+}
+
+class MyHouse extends House {
+  super:House;
+
+  openDoor(key: Key): void {
+    if (key.getSignature()) {
+      this.door = true;
+      console.log(`Key: ${key.getSignature()} open the door`);
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
